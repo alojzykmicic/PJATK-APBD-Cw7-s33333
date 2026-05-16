2fd792e9e167de3ef2ea2285d7be59e7bfc7a917
Wojciech Sokołowski <s33333@pjwstk.edu.pl>
2026-05-16 16:00:09 +0200

    .

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,17 +1,23 @@
+using WebApplication2.Services;
+using WebApplication2.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<IPCService, PCService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
